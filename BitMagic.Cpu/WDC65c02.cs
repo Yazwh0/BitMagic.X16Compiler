d2a53cda6a8e95b659024927fecfb8fa8e8289d3
Yazwh0 <90315107+Yazwh0@users.noreply.github.com>
2023-02-24 18:20:08 +0000

    Update WDC65c02.cs

--- BitMagic.Cpu/WDC65c02.cs
+++ BitMagic.Cpu/WDC65c02.cs
@@ -5,8 +5,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace BitMagic.Cpu
 {
